6ea6c987405d727244d8b2111faf720c5a9be99c
JavierBertolino <bertolino.javier@gmail.com>
2025-03-07 02:09:20 -0300

    Configure for static export

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'export',  // Enable static exports
   trailingSlash: true,  // Add trailing slashes to all URLs
   images: {
     unoptimized: true, // Required for static export
   },
-};
+}
 
-export default nextConfig;
+module.exports = nextConfig 
